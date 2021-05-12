6d0f2986fb31de08a435b85a6cc2457f70e1c504
melanie grisey <melanie.grisey@hotmail.fr>
2021-05-12 17:59:04 +0200

    seed 5 new restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Restaurant.destroy_all
 puts 'Creating 5 fake restaurants...'
 5.times do
   restaurant = Restaurant.new(
-    name:    Faker::Company.name,
+    name:    Faker::Restaurant.name,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     phone_number: Faker::PhoneNumber.cell_phone,
     category: Restaurant::CATEGORIES.sample
